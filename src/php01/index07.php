d28df9c68acd9d1c8fa262e8979736a11133e951
sugita yuya <yuya16578@gmail.com>
2023-11-22 11:37:14 +0900

    リモートリポジトリの変更

---
+++ src/php01/index07.php
@@ -0,0 +1,26 @@
+<?php
+$people = [
+  [
+    "name" => "Taro",
+    "age" => 25,
+    "gender" => "men"
+  ],
+  [
+    "name" => "Jiro",
+    "age" => 20,
+    "gender" => "men"
+  ],
+  [
+    "name" => "hanako",
+    "age" => 16,
+    "gender" => "women"
+  ]
+];
+
+foreach ($people as $person) {
+  echo $person["name"] . "(" . $person["age"] . "歳" . $person["gender"] . ")";
+  echo ' ';
+}
+
+echo '<br />';
+var_dump($people);
